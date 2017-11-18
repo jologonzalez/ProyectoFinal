85aacce7cb75eaa2c3c184ccebe98b5d3298114a
jologonzalez <jorge.lopezgo@campusucc.edu.co>
2017-11-18 10:10:27 -0500

    V2

--- app/src/main/java/com/example/jorgelopez/proyectofinal/Vistas/Actividades/PrincipalActivity.java
+++ app/src/main/java/com/example/jorgelopez/proyectofinal/Vistas/Actividades/PrincipalActivity.java
@@ -1,6 +1,9 @@
 package com.example.jorgelopez.proyectofinal.Vistas.Actividades;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -17,6 +20,9 @@ public class PrincipalActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,12 @@ public class PrincipalActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Perfil"));
+        tabLayout.addTab(tabLayout.newTab().setText("Mensajes Recividos"));
+        tabLayout.addTab(tabLayout.newTab().setText("Mensajes Enviados"));
+
+        tabLayout.setTabTextColors(ColorStateList.valueOf(Color.WHITE));
 
     }
 
